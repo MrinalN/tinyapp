713bbff6a4af871167195591736faca19340be6e
Adele Noronha <adele.c.noronha@gmail.com>
2021-03-15 21:59:21 +0000

    urls_new.ejs template and functionality

--- express_server.js
+++ express_server.js
@@ -18,11 +18,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   console.log(req);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
