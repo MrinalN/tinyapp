9f4a7a899733ea8af37a03bc803724d79dce139c
Adele Noronha <adele.c.noronha@gmail.com>
2021-03-15 23:32:04 +0000

    shortyURL generated successfully upon user input

--- express_server.js
+++ express_server.js
@@ -30,8 +30,11 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok")   // Respond with 'Ok' (we will replace this)
+  console.log(req.body["longURL"]);  // Log the POST request body to the console
+  let longURL = req.body["longURL"];
+  let shortyURL = generateRandomString();
+  urlDatabase[shortyURL] = longURL;
+  res.redirect(`/urls/${shortyURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
