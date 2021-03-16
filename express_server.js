c063dbaa826e190fe96792447fbbe9d980b66416
Adele Noronha <adele.c.noronha@gmail.com>
2021-03-16 19:04:55 +0000

    put added, delete and redirects working

--- express_server.js
+++ express_server.js
@@ -26,15 +26,16 @@ app.get("/", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
+  console.log("THIS IS TEMPVARS:",templateVars);
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body["longURL"]);  // Log the POST request body to the console
+  //console.log(req.body["longURL"]);  // Log the POST request body to the console
   let longURL = req.body["longURL"];
-  let shortyURL = generateRandomString();
-  urlDatabase[shortyURL] = longURL;
-  res.redirect(`/urls/${shortyURL}`);
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -47,15 +48,20 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(urlDatabase)//tester
+  console.log(req.params.shortURL)
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
