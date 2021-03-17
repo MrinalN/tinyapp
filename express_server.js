3a3788c5d8763c6f7c030e0f114eb83a50de0963
Adele Noronha <adele.c.noronha@gmail.com>
2021-03-17 22:09:37 +0000

    Login pg created. Header adjusted.

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ app.use(cookieParser());
 
 const PORT = 8080;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 //IN MEMORY
@@ -17,15 +17,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
@@ -33,7 +33,7 @@ const users = {
 //HELPER FUNCTIONS
 function generateRandomString() {
   let result = "";
-  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
@@ -44,6 +44,20 @@ const updateUrlDatabase = (shortURL, content) => {
   urlDatabase[shortURL] = content;
 };
 
+const findUserByEmail = (email) => {
+  // loop and try to match the email
+  for (let user in users) {
+    const userObj = users[user];
+
+    if (userObj.email === email) {
+      // if found return the user
+      return userObj;
+    }
+  }
+
+  // if not found return false
+  return false;
+};
 //END POINTS OR ROUTES
 
 //basic homepage (choice to redirect to urls instead)
@@ -54,7 +68,7 @@ app.get("/", (req, res) => {
 
 //collects info from login bar, sets email cookie
 app.post("/login", (req, res) => {
-  const templateVars = {"user_id":req.body.email};
+  const templateVars = { "user_id": req.body.email };
   res.cookie("user_id", templateVars);
   res.redirect("/urls");
 });
@@ -69,6 +83,14 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/login", (req, res) => {
+  userID = req.cookies['user_id']
+  const templateVars = {
+    user: users[userID],
+  }
+  res.render("login", templateVars);
+});
+
 //takes in info from registration input, renders to register.ejs
 app.get("/register", (req, res) => {
   userID = req.cookies['user_id']
@@ -78,17 +100,31 @@ app.get("/register", (req, res) => {
   res.render("register", templateVars);
 });
 
+//**CONSOLE LOG PRINTING TWICE FOR SOME REASON */
 //New user - registers ID, sets cookie, adds to database
 app.post("/register", (req, res) => {
-  const {email, password} = req.body;
-  const id = generateRandomString();
-  users[id] = {
-    id,
-    email,
-    password
-  };
-res.cookie("user_id", id)
-res.redirect("/urls");
+  const { email, password } = req.body;
+  const foundUser = findUserByEmail(email);
+
+  if (foundUser) {
+    res.status(400).send('Email already registered. Log in please!')
+    //console.log(users);
+  }
+
+  if (email && password && !foundUser) {
+    const id = generateRandomString();
+    users[id] = {
+      id,
+      email,
+      password
+    };
+    //console.log(users);
+    res.cookie("user_id", id)
+    res.redirect("/urls");
+  } else {
+    //console.log(users);
+    res.status(400).send('Please input an email address')
+  }
 });
 
 //takes in info from registration input, renders to register.ejs
@@ -98,7 +134,6 @@ app.get("/urls", (req, res) => {
     urls: urlDatabase,
     user: users[userID]
   };
-  console.log(userID)
   res.render("urls_index", templateVars);
 });
 
@@ -124,9 +159,11 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { 
-    shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL] 
+  userID = req.cookies['user_id']
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    user: users[userID]
   };
   res.render("urls_show", templateVars);
 });
